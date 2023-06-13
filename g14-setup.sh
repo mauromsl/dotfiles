7f8a9585e629b71b59ee33760e39eb1cbcd29228
Mauro MSL <mauro.msl@outlook.com>
2023-06-13 21:32:23 +0200

    G14: Update setup for Kernel 6.1+

--- g14-setup.sh
+++ g14-setup.sh
@@ -1,22 +1,34 @@
 # Arch setup script for the G14 on Kernel 5.9
 
 # Custom asus daemon
-pacman -S asus-nb-ctrl-git
+#pacman -S asus-nb-ctrl-git
 
 # Setup custom kernel should no longer be required from 5.11
-cat >> /etc/pacman.conf << EOM
+# cat >> /etc/pacman.conf << EOM
 
-[g14]
-SigLevel = Optional TrustAll
-Server = https://arch.asus-linux.org
-EOM
-pacman -Sy
-pacman -S linux-g14 linux-g14-headers
+# [g14]
+# SigLevel = Optional TrustAll
+# Server = https://arch.asus-linux.org
+# EOM
+# pacman -Sy
+# pacman -S linux-g14 linux-g14-headers
 
 # Disable conflicting drivers
-cat >> /etc/modprobe.d/blacklist.conf << EOM
-blacklist nouveau
-blacklist nvidiafb
-blacklist rivafb
-blacklist i2c_nvidia_gpu
-EOM
+# cat >> /etc/modprobe.d/blacklist.conf << EOM
+# blacklist nouveau
+# blacklist nvidiafb
+# blacklist rivafb
+# blacklist i2c_nvidia_gpu
+# EOM
+#
+# KERNEL 6.1 +
+# https://wiki.archlinux.org/title/ASUS_Linux
+#
+# from AUR, install asusctl
+pacman -Sy asusctl
+
+# change the optimus cpu profile, for example, if you want to use integrated gfx only:
+supergfxctl --mode Integrated
+
+#Change the power profile:
+asusctl profile -P Quiet
